370ad20e9666c3754a8f0b3c3e8c1ce0ee8502aa
Henning Deeken <hdeeken@uos.de>
2014-01-24 11:17:39 +0100

    cleaned .h

--- 3d_nav_executive/include/3d_nav_executive/arm.h
+++ 3d_nav_executive/include/3d_nav_executive/arm.h
@@ -12,47 +12,44 @@
 #include <kinematics_msgs/GetKinematicSolverInfo.h>
 #include <kinematics_msgs/GetPositionFK.h>
 
-
 using namespace std;
 
 typedef actionlib::SimpleActionClient< pr2_controllers_msgs::JointTrajectoryAction > TrajClient;
 
 class Arm
 {
-  public: 
-    
-    Arm(std::string arm_name);
-    ~Arm();
+	public: 
 
-    void stopArm();
+	Arm(std::string arm_name);
+	~Arm();
 
-    void sendArmToPose(double pose[], double move_time);
-    void sendArmToPoseQuaternion(double pose[], double move_time);
-    void sendArmToPoses(std::vector<std::vector<double> > &poses, std::vector<double> move_times);
+	void stopArm();
 
-    void sendArmToConfiguration(double configuration[], double move_time);
-    void sendArmToConfigurations(std::vector<std::vector<double> > &configurations, std::vector<double> move_times);
-    void sendArmToConfiguration(std::vector<float> configuration, double move_time);
+	void sendArmToPose(double pose[], double move_time);
+	void sendArmToPoseQuaternion(double pose[], double move_time);
+	void sendArmToPoses(std::vector<std::vector<double> > &poses, std::vector<double> move_times);
 
-    bool computeIK(const geometry_msgs::Pose &pose, std::vector<double> jnt_pos, std::vector<double> &solution);
-    bool performFK(const std::vector<double> jnt_pos, std::vector<double> &cart_pose);
+	void sendArmToConfiguration(double configuration[], double move_time);
+	void sendArmToConfigurations(std::vector<std::vector<double> > &configurations, std::vector<double> move_times);
+	void sendArmToConfiguration(std::vector<float> configuration, double move_time);
 
-    void getCurrentArmConfiguration(vector<double>& current_angles);
-    void getCurrentArmPose(vector<double>& cpose);
+	bool computeIK(const geometry_msgs::Pose &pose, std::vector<double> jnt_pos, std::vector<double> &solution);
+	bool performFK(const std::vector<double> jnt_pos, std::vector<double> &cart_pose);
+
+	void getCurrentArmConfiguration(vector<double>& current_angles);
+	void getCurrentArmPose(vector<double>& cpose);
 
   private:
-    ros::NodeHandle nh_;
-    //ros::Subscriber joint_states_subscriber_;
-  
-    std::string arm_name_;
-    std::string ik_service_name_;
-    std::string fk_service_name_;
-    std::vector<std::string> joint_names_;
-    std::string reference_frame_;
+	ros::NodeHandle nh_;
+	//ros::Subscriber joint_states_subscriber_;
 
-    std::string controller_state_name_;
+	std::string arm_name_;
+	std::string ik_service_name_;
+	std::string fk_service_name_;
+	std::vector<std::string> joint_names_;
+	std::string reference_frame_;
 
-    TrajClient* traj_client_;
+	std::string controller_state_name_;
+
+	TrajClient* traj_client_;
 };
-
-
